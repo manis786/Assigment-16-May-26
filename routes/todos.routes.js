4544c6d3d9b117ce3dbcd8908ed4e44e35b3f527
manis786 <anis.siddiq@gmail.com>
2026-05-19 02:32:33 +0500

    Update Work Done

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -1,33 +1,16 @@
-import express from "express"
+import express, { Router } from "express"
 import Todos from "../models/todos.models.js"
+import { addTodos, getAllTodos, getTodobyId ,updateTodobyId } from "../controllers/todo.controller.js"
 
 const router = express.Router()
-router.get("/", async (req,res)=>{
-    try {
-        const todos = await Todos.find()
-        console.log(`Todos ==>> ${todos}`)
-        res.status(200).json({
-            status:true,
-            message:"Todos Fetched Successfully",
-            data:todos
-        })
-    } catch (error) {
-        console.log(`Error ${error}`)
-    }
+router
+  .route("/")
+  .get(getAllTodos)
+  .post(addTodos)
+router
+.route("/:todoId")
+.get(getTodobyId)
+.put(updateTodobyId)
 
-})
-router.post("/",async (req,res)=>{
-    try {
-        console.log("Create Todos =>",req.body)
-        const response = await Todos.create(req.body)
 
-        res.status(200).json({
-            status : true,
-            message: "Todo Created Successfully",
-            data : null
-        })
-    } catch (error) {
-        console.log(`Error Occured While Creating Todo ${error}`)
-    }
-})
 export default router
